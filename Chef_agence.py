1ecf47ad482424fed62804cc736189c358274c8d
Taïga fl <56323049+TJ345@users.noreply.github.com>
2019-11-04 00:09:04 +0100

    Update Chef_agence.py

--- Chef_agence.py
+++ Chef_agence.py
@@ -1,8 +1,22 @@
+"""MAITAIGAHASSE AHIDJO Mat: 18A041FS"""
+
 class Chef_agence:
-     """classe représentant le Chef d'agence d'une banque"""
-     def__init__(self,Id_ChefA,Nom,Prenom,Age,Sexe):
+     """classe définissant le Chef d'agence de la banque représenté par:
+        -son Identifiant
+        -son nom
+        -son prenom
+        -sa date de naissance
+        -son sexe"""
+     
+     def__init__(self,Id_ChefA,Nom,Prenom,Date_naissance,Sexe):
+          """constructeur de notre classe"""
         self.Id_ChefA=Identifiant
         self.Nom=Nom
         self.Prenom=Prenom
-        self.Age=Age
+        self.Date_naissance=Date_naissance
         self.Sexe=Sexe
+     
+     def __repr__( self) :
+          """Quand on entre notre objet dans l'interpréteur"""
+      return "Chef_agence: Identifiant( { } ) , Nom( { } ) , Prenom( { } ) , Date_naissance( { } ) , Sexe( { } ) ". format(
+     self. Identifiant, self. Nom, self. Prenom self. Date_naissance, self. Sexe)
