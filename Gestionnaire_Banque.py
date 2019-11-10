8b67ab9467308dfcbd00455137bd3f6c7dae53e5
Innoss <56593254+Innoss@users.noreply.github.com>
2019-11-10 20:52:17 +0100

    Add files via upload
    17B463FS

---
+++ Gestionnaire_Banque.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  3 16:37:54 2019
+
+@author: DIEZOUMBE
+"""
+
+class Gestionnaire:
+    def __init__(self, c_emprunt, c_compte, ):
+        print("quelle opération souhaitez-vous effectuer ? ")
+        self.emprunt = "emprunt"
+        self.compte = "compte"
+
+print("lancement du proramme")
+
+g = Gestionnaire("emprunt", "compte")
+print("operation1 de g  : {}".format(g.emprunt))
+print("operation2 de g  : {}".format(g.compte))
+g_id = input("entrez votre choix : ")
+  self.operation1 = emprunt
+  self.operation2 = compte   
+   if choix == operation1 : 
+def enregistrer(self, emprunt) :
+    print("l_emprunt du client{}".format(self.nom_client, emprunt))
+    else 
+    if choix == operation2:
+        def creer_compte(self, nom_client)*:
+            else:
+                def modifier_compte(self, modification_compte):
+                    else:
+                        def supprimer_compte(self, suppression_compte):
+     print ("operations {} effectuee avec succes ")                
+    
