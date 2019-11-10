4749301d5601e93c1b4fc141b399a2899d81e99d
19-Eu <56972305+19-Eu@users.noreply.github.com>
2019-11-10 10:43:43 +0100

    Create compteepargne.py

---
+++ compteepargne.py
@@ -0,0 +1,12 @@
+#AWONO ATANGANA Eugène Géraldin mat: 18A869FS
+ 
+ class compte_epargne(Compte):
+      def __init__(self,n,compte,virement)
+          self.emprunt=virement
+          
+      def _get_virement(auto,somme)
+          print ("votre solde restant est de {0} \ n".format (self._somme))
+          
+      def _set_vire ment (auto,somme)
+          print ("votre solde restant est de {0} \ n".format (self._somme)
+          moi=property
