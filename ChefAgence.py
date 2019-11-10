cc582f252ec7cd03d0f642c6c974c744f41920d8
Taïga fl <56323049+TJ345@users.noreply.github.com>
2019-11-10 19:48:27 +0100

    Create ChefAgence.py

---
+++ ChefAgence.py
@@ -0,0 +1,15 @@
+"""MAITAIGAHASSE AHIDJO MATRICULE:18A041FS"""
+
+import Personne
+
+class Chef_agence(Personne):
+ 
+ def commande(self,nom,prenom,identifiant,commande):
+ 
+ Personne__init__(self,nom,prenom,identifiant)
+ 
+    self.commande=command
+    
+    
+      return command
+      
