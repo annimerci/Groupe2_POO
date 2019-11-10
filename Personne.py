d6522a35cf96895386bbcbc58dceafc5e14fb2ea
Capri12 <56361166+Capri12@users.noreply.github.com>
2019-11-10 19:07:21 +0100

    Add files via upload

---
+++ Personne.py
@@ -0,0 +1,9 @@
+
+
+#LABARAN ABOUBAKAR 18A090FS // lien video : https://youtu.be/PsTdNXxROmc
+
+class Personne:
+    def __init__(self,nom,prenom,identifiant):
+        self.nom=nom
+        self.prenom=prenom
+        self.identifiant=identifiant
